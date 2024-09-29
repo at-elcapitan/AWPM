6f29bfc9a333eb9bda95c1077cba046d113eb49c
at-elcapitan <gameforceman1808@gmail.com>
2024-09-29 10:40:19 +0300

    Added parser for future package files

---
+++ Parser.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace AWPM
+{
+    public class SyntaxErrorException : Exception
+    {
+        public List<(int, string)> DataList { get; }
+
+        public SyntaxErrorException(List<(int, string)> dataList)
+            : base("An error occurred with the provided data.")
+        {
+            DataList = dataList;
+        }
+    }
+
+    internal class Parser
+    {
+        private Regex variablePattern = new Regex(@"(\w+)\s*=\s*""([^""]*)""");
+        private Regex arrayPattern = new Regex(@"(\w+)\s*=\s*\[\s*((?:\s*""[^""]*""(?:,\s*)?)+)\s*\]");
+
+        private Dictionary<string, string> variables =       
+                            new Dictionary<string, string>();
+        private Dictionary<string, List<string>> lists =
+                            new Dictionary<string, List<string>>();
+        private List<(int line, string text)> notMatching =
+                            new List<(int line, string text)>();
+
+        public Dictionary<string, string> Variables
+        {
+            get => variables;
+        }
+
+        public Dictionary<string, List<string>> Lists
+        {
+            get => lists;
+        }
+
+        private bool syntaticCheck(string[] text)
+        {
+            bool isValidated = true;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                string line = text[i];
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                //for (int j = i + 1; j < text.Length; j++)
+                //{
+                //    if (!line.TrimEnd().EndsWith("\\", StringComparison.Ordinal))
+                //    {
+                //        i = j;
+                //        break;
+                //    }
+
+                //    line = line.Remove(line.Length - 1);
+                //    line += text[j].TrimStart();
+                //}
+
+
+                if (line.Contains("[") && !line.Contains("]"))
+                {
+                    bool arrayValid = false;
+
+                    for (int j = i + 1; j < text.Length; j++)
+                    {
+                        if (text[j].Contains("="))
+                        {
+                            break;
+                        }
+
+                        if (text[j].Contains("]"))
+                        {
+                            line += text[j].TrimStart();
+                            i = j;
+                            arrayValid = true;
+                            break;
+                        }
+
+                        line += text[j].TrimStart();
+                    }
+
+                    if (!arrayValid)
+                    {
+                        notMatching.Add((i + 1, text[i].TrimStart() + 
+                                        " --- [ was never closed"));
+                        isValidated = false;
+                        break;
+                    }
+                }
+
+                if (!line.Contains("[") && line.Contains("]"))
+                {
+                    notMatching.Add((i + 1, text[i].TrimStart() + 
+                                        " <- unexpected token"));
+                    isValidated = false;
+                    break;
+                }
+
+                bool match = false;
+
+                if (this.variablePattern.IsMatch(line))
+                {
+                    match = true;
+                    continue;
+                }
+
+                if (this.arrayPattern.IsMatch(line))
+                {
+                    match = true;
+                    continue;
+                }
+
+                if (!match)
+                {
+                    notMatching.Add((i + 1, line.TrimStart() 
+                        + $" <- unexpected token(s) starting {i + 1} line"));
+                    isValidated = false;
+                    break;
+                }
+            }
+
+            return isValidated;
+        }
+
+        private void parseData(string text)
+        {
+            foreach (Match match in this.variablePattern.Matches(text))
+            {
+                this.variables[match.Groups[1].Value] = match.Groups[2].Value
+                                                                  .Trim('"');
+            }
+
+            foreach (Match match in arrayPattern.Matches(text))
+            {
+                List<string> items = new List<string>();
+
+                var itemMatches = Regex.Matches(match.Groups[0].Value, "\"([^\"]*)\"");
+
+                foreach (Match itemMatch in itemMatches)
+                {
+                    items.Add(itemMatch.Groups[1].Value);
+                }
+
+                lists[match.Groups[1].Value] = items;
+            }
+        }
+
+#if DEBUG
+        public Parser(string data)
+        {
+            if (!this.syntaticCheck(data.Split('\n')))
+            {
+                throw new SyntaxErrorException(this.notMatching);
+            }
+            this.parseData(data);
+        }
+#else
+        //public Parser(string relativePath)
+        //{
+        //    if(!this.validateData(File.ReadAllLines(relativePath)))
+        //    {
+        //        throw new FormatException("Found some unvalid strings");
+        //    }
+
+        //    this.parseData(File.ReadAllText(relativePath));
+        //}
+#endif
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,55 +8,99 @@ namespace AWPM
     {
         public static void Main(string[] args)
         {
-            ProgramFlags flags;
+            //            ProgramFlags flags;
+
+            //            try
+            //            {
+            //                flags = new ProgramFlags(args);
+            //            }
+            //            catch (Exception e)
+            //            {
+            //                Console.WriteLine(e.Message);
+            //                return;
+            //            }
+
+            //#if DEBUG
+            //            Console.Write(flags);
+            //#endif
+
+            //    if (flags.Version)
+            //    {
+            //        Version version = Assembly.GetExecutingAssembly()
+            //                                 .GetName().Version;
+            //        Console.WriteLine($"Ado Windows Package Manager v" +
+            //            version + "\n\nThis is free software; see the source for " +
+            //            	"copying conditions. There is NO\nwarranty; not even " +
+            //            	"for MERCHANTABILITY or FITNESS FOR A PARTICULAR " +
+            //            	"PURPOSE.\n\nWritten by Vladislav 'ElCapitan' Nazarov");
+            //        return;
+            //    }
+
+            //    (bool, string) valid = flags.validateArguments;
+
+            //    if (!valid.Item1)
+            //    {
+            //        Console.WriteLine($"Error: {valid.Item2}\n");
+            //        Console.WriteLine("Command failed, exiting...");
+            //        return;
+            //    }
+
+            //    List<Operations> ops = flags.generateTasklist();
+
+            //    foreach (Operations operation in ops)
+            //    {
+            //        Console.WriteLine(operation);   
+            //    }
+
+            //    foreach (string s in flags.Packages)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+#if DEBUG
+            string input = @"
+                    test = ""45""
+                    type            = ""bin""
+                    program_name    = ""pr ew fer erg rogram""
+                    program_author  = ""elcapitan""
+                    program_site    = ""https://github.com/at-elcapitan/AWPM""
+                    program_version = ""1.1.1""
+                    optional_deps   = [
+                            ""progr:K|am3 : something"", 
+                            ""1231,2""
+                            ]
+                  ";    
+            Parser parser;
 
             try
             {
-                flags = new ProgramFlags(args);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return;
-            }
+                parser = new Parser(input);
 
-#if DEBUG
-            Console.Write(flags);
+                foreach (var kvp in parser.Variables)
+                {
+                    Console.WriteLine($"{kvp.Key} = {kvp.Value}");
+                }
+
+                foreach(var kvp in parser.Lists)
+                {
+                    Console.Write($"{kvp.Key} = [ ");
+
+                    foreach (string s in kvp.Value)
+                    {
+                        Console.Write($"\"{s}\" ");
+                    }
+
+                    Console.Write("]\n");
+                }
+            } catch (SyntaxErrorException e)
+            {
+                Console.WriteLine("Syntax error");
+
+                foreach((int, string) errline in e.DataList)
+                {
+                    Console.Write($"- line {errline.Item1}: {errline.Item2}");
+                }
+            }
 #endif
-
-            if (flags.Version)
-            {
-                Version version = Assembly.GetExecutingAssembly()
-                                         .GetName().Version;
-                Console.WriteLine($"Ado Windows Package Manager v" +
-                    version + "\n\nThis is free software; see the source for " +
-                    	"copying conditions. There is NO\nwarranty; not even " +
-                    	"for MERCHANTABILITY or FITNESS FOR A PARTICULAR " +
-                    	"PURPOSE.\n\nWritten by Vladislav 'ElCapitan' Nazarov");
-                return;
-            }
-
-            (bool, string) valid = flags.validateArguments();
-
-            if (!valid.Item1)
-            {
-                Console.WriteLine($"Error: {valid.Item2}\n");
-                Console.WriteLine("Command failed, exiting...");
-                return;
-            }
-
-            List<Operations> ops = flags.generateTasklist();
-
-            foreach (Operations operation in ops)
-            {
-                Console.WriteLine(operation);   
-            }
-                
-            foreach (string s in flags.Packages)
-            {
-                Console.WriteLine(s);
-            }
-
         }
     }
 }
